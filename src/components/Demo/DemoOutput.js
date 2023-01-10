ae57c8ffa5f8dd0ed7b8b53942155966079385f2
Harish1651999 <hk1651999@gmail.com>
2023-01-10 21:05:43 +0530

    add react fragment.

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -2,7 +2,11 @@ import React from "react";
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
-  return <MyParagraph>{props.show ? "This is new" : ""}</MyParagraph>;
+  return (
+    <>
+      <MyParagraph>{props.show ? "This is new" : ""}</MyParagraph>
+    </>
+  );
 };
 
 export default React.memo(DemoOutput);
