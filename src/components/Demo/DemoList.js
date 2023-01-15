ab217f8a85c79db334d40e6eb6335baaec79e470
Harish1651999 <hk1651999@gmail.com>
2023-01-15 21:27:07 +0530

    add demoList content.

--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -2,7 +2,14 @@ import React from "react";
 import classes from "./DemoList.module.css";
 
 const DemoList = () => {
-  return <></>;
+  return (
+    <div className={classes.list}>
+      <h2>My Title</h2>
+      <ul>
+        <li>1</li>
+      </ul>
+    </div>
+  );
 };
 
 export default DemoList;
