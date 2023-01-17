822b5c5c0b5bd918dc0ff6f297af4bf4ec97c788
Harish1651999 <hk1651999@gmail.com>
2023-01-17 21:44:17 +0530

    add demoList sorting.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Button from "./components/UI/Button/Button";
 function App() {
   const [listTitle, setListTitle] = useState("My List");
 
-  const titleChangeHandler = () => {
+  const titleChangeHandler = useCallback(() => {
     setListTitle("New Title");
-  };
+  }, []);
 
   return (
     <div className="App">
-      <DemoList title={listTitle} />
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
       <Button onClick={titleChangeHandler}>Change List Title</Button>
     </div>
   );
--- src/components/Demo/DemoList.js
+++ src/components/Demo/DemoList.js
@@ -2,11 +2,14 @@ import React from "react";
 import classes from "./DemoList.module.css";
 
 const DemoList = (props) => {
+  const sortedList = props.items.sort((a, b) => a - b);
   return (
     <div className={classes.list}>
       <h2>{props.title}</h2>
       <ul>
-        <li>1</li>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </div>
   );
